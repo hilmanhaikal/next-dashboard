91223f90cce461b90c8cb1f5c5c4ca3979926a6e
hilmanhaikal <hilmanhaikal7@gmail.com>
2025-02-22 08:02:28 +0800

    Specify Node.js engine version in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  engines: {
+    node: "18",
+  },
   images: {
     remotePatterns: [
       {
